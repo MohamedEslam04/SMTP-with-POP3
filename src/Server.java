c78d553ab861d87e6ef19af79cda36c52290d90b
Mohamed Eslam Amin <60846785+MohamedEslam04@users.noreply.github.com>
2023-12-06 22:07:11 +0200

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,3 @@
-// Java implementation of Server side 
-// It contains two classes : Server and ClientHandler 
-// Save file as Server.java 
-
 import java.io.*;
 import java.net.*;
 
@@ -11,18 +7,21 @@ public class Server {
     public static void main(String[] args) throws IOException {
         final int port = 5056;
         final int maxbacklog = 1000;
-        // server is listening on port 5056 
+
+        // Server is listening on port 5056 
         InetAddress ip = InetAddress.getByName("localhost");
         ServerSocket socket = new ServerSocket(port, maxbacklog, ip);
+        
+        // Create credentials file
         FileManager.createCredentialsFile();
-        // running infinite loop for getting 
-        // client request 
+        
+        // Running infinite loop for getting client request 
         while (true) {
             System.out.println(ip + " server with port number " + port + " is booted up.");
             Socket s = null;
 
             try {
-                // socket object to receive incoming client requests 
+                // Socket object to receive incoming client requests 
                 s = socket.accept();
 
                 System.out.println("A new client is connected : " + s);
@@ -30,7 +29,7 @@ public class Server {
                 PrintWriter outputStream = new PrintWriter(s.getOutputStream());
                 System.out.println("Assigning new thread for this client");
 
-                // create a new thread object 
+                // Create a new thread object 
                 Thread t = new ClientHandler(s, inputStream, outputStream);
 
                 // Invoking the start() method 
