642787b02ac67ed5deda1214d302f22a69fb7229
Mohamed Eslam Amin <60846785+MohamedEslam04@users.noreply.github.com>
2023-12-06 22:02:36 +0200

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,3 @@
-// Java implementation for a client 
-// Save file as Client.java 
-
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -11,27 +8,26 @@ public class Client {
     public static void main(String[] args) throws IOException {
         final int port = 5056;
         String received;
+
         try {
             Scanner scanner = new Scanner(System.in);
 
-            // getting localhost ip 
+            // Getting localhost IP
             InetAddress ip = InetAddress.getByName("localhost");
 
-            // establish the connection with server port 5056 
+            // Establish the connection with the server on port 5056
             Socket socket = new Socket(ip, port);
 
             BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter outputStream = new PrintWriter(socket.getOutputStream());
 
-            // obtaining input and out streams 
-//			DataInputStream inputStream = new DataInputStream(socket.getInputStream()); 
-//			DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream()); 
-            // the following loop performs the exchange of 
-            // information between client and client handler 
+            // The following loop performs the exchange of information between client and client handler
             while (true) {
                 received = inputStream.readLine();
                 System.out.println(received);
+
                 if (received.contains("Input")) {
+                    // User input
                     String sendmesg = scanner.nextLine();
                     outputStream.println(sendmesg);
                     outputStream.flush();
@@ -40,13 +36,10 @@ public class Client {
                         break;
                     }
                 }
-                // If client sends exit,close this connection 
-                // and then break from the while loop 
-
             }
 
-            // closing resources 
-            System.out.println("Closing this connection : " + socket);
+            // Closing resources
+            System.out.println("Closing this connection: " + socket);
             System.out.println("Connection closed");
             scanner.close();
             inputStream.close();
